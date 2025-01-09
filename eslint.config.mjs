4c08b2d156e403fd46ce2e034edfdb5209eaabe9
Lara Salameh <hello@larasalameh.com>
2025-01-09 18:09:51 +1100

    ES module format

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,8 @@
-// .eslintrc.js
-export default {
+// eslint.config.mjs
+import { FlatCompat } from "@eslint/eslintrc";
+
+const compat = new FlatCompat();
+
+export default compat.config({
   extends: ["next/core-web-vitals"],
-};
+});
