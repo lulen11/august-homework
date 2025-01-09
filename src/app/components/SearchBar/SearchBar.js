f9b849a7fc31dc77fa2c1a63ae7c1ff99ff714a9
Lara Salameh <hello@larasalameh.com>
2025-01-09 19:05:26 +1100

    testt

--- src/app/components/SearchBar/SearchBar.js
+++ src/app/components/SearchBar/SearchBar.js
@@ -4,9 +4,9 @@ export default function SearchBar({ query, setQuery, handleSearch }) {
   return (
     <>
       <h1 className={styles.heading}>
-        Hi August Team! Here&apos;s a little application that allows you to
-        search the YouTube catalogue, play a video, and save a video for later!
-        Start searching now!
+        Hi August! Here&apos;s a little application that allows you to search
+        the YouTube catalogue, play a video, and save a video for later! Start
+        searching now!
       </h1>
       <form className={styles.searchBar} onSubmit={handleSearch}>
         <input
