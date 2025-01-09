4d446ac005d43fd689eaa7aa80ddfcc297497108
Lara Salameh <hello@larasalameh.com>
2025-01-09 18:08:11 +1100

    eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,21 +1,4 @@
-// import { dirname } from "path";
-// import { fileURLToPath } from "url";
-// import { FlatCompat } from "@eslint/eslintrc";
-
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = dirname(__filename);
-
-// const compat = new FlatCompat({
-//   baseDirectory: __dirname,
-// });
-
-// const eslintConfig = [...compat.extends("next/core-web-vitals")];
-
-// export default eslintConfig;
-import { FlatCompat } from "@eslint/eslintrc";
-
-const compat = new FlatCompat();
-
-export default compat.config({
+// .eslintrc.js
+export default {
   extends: ["next/core-web-vitals"],
-});
+};
